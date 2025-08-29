01bc3c18eff0d69a70a628b28b0def38c06068e9
Clickadelic <toby.hopp@gmail.com>
2025-08-29 14:40:47 +0200

    Cntrlrs

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,49 @@
 
 namespace App\Models;
 
+use App\Http\Requests\StorePostRequest;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function index()
+    {
+        //
+    }
+
+    public function show(Post $post)
+    {
+        
+    }
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(StorePostRequest $request)
+    {
+        //
+    }
+
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    // public function update(StorePostRequest $request, Post $post)
+    // {
+    //     //
+    // }
+
+    // public function destroy(Post $post)
+    // {
+    //     //
+    // }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,6 +15,9 @@ Route::get('/dashboard', function () {
 
 // Posts
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+
+// Users
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
